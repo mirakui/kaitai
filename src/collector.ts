9e7eb888770b3aa2179e38f88e6c71fcd0a10d98
Issei Naruta <naruta@cookpad.com>
2020-03-21 01:32:37 +0900

    real headers

--- src/collector.ts
+++ src/collector.ts
@@ -35,12 +35,21 @@ async function setupPage(browser: puppeteer.Browser) {
       return;
     }
     let headers = request.headers();
-    headers["connection"] = "keep-alive";
-    headers["user-agent"] =
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/82.0.4076.0 Safari/537.36";
-    headers["accept-language"] = "en,ja;q=0.9";
-    headers["accept"] =
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+    headers = {
+      Connection: "keep-alive",
+      Pragma: "no-cache",
+      "Cache-Control": "no-cache",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4088.0 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-User": "?1",
+      "Sec-Fetch-Dest": "document",
+      "Accept-Language": "en,ja;q=0.9"
+    };
     request.continue({ headers });
   });
 
